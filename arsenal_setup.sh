7aefb87878b7d7c35a0727880c739a4570775486
Berk Cem Göksel <berkcgoksel@gmail.com>
2018-12-19 15:11:08 +0300

    Update arsenal_setup.sh

--- arsenal_setup.sh
+++ arsenal_setup.sh
@@ -5,7 +5,7 @@ case "$response" in
 	mkdir /etc/ssh/default_kali_keys
 	mv /etc/ssh/ssh_host_* /etc/ssh/default_kali_keys/
 	dpkg-reconfigure openssh-server
-	echo "Verify if the SSH keys have changed or not!"
+	echo "You might want to verify if the SSH keys have changed(!)"
 	echo
 	echo New keys:
 	md5sum /etc/ssh/ssh_host_*
@@ -13,10 +13,9 @@ case "$response" in
 	echo Old keys:
 	md5sum /etc/ssh/default_kali_keys/*
 	echo
-	read -n 1 -s -r -p "Press any key to continue"
+	read -n 1 -s -r -p "Press any key to continue\n"
         ;;
     *)
-        continue
         ;;
 esac
 
@@ -29,6 +28,8 @@ git clone https://github.com/hugsy/gef.git /usr/share/arsenal/GEF
 git clone https://github.com/berkgoksel/opener.git /usr/share/arsenal/opener
 git clone https://github.com/Ganapati/RsaCtfTool.git /usr/share/arsenal/RsaCtfTool
 git clone https://github.com/Nekmo/dirhunt.git /usr/share/arsenal/dirhunt
+git clone https://github.com/pwndbg/pwndbg /usr/share/arsenal/pwndbg
+
 
 apt-get install -y python3-pip
 apt-get install -y gobuster
@@ -38,7 +39,8 @@ apt-get install -y python-pip
 pip3 install netaddr
 pip install capstone
 pip install pwntools
-
+pip install ropper
+pip install unicorn
 
 echo "
 alias elayv='python3 /usr/share/arsenal/elayv/elayv.py'
@@ -55,6 +57,65 @@ alias msfpescan='python /usr/share/framework2/msfpescan'
 
 source /root/.bashrc
 
- 
+
 #Uniq duplicate alias(?) entries - UNTESTED
 #cat /root/.bashrc | nl|sort -k 2|uniq -f 1|sort -n|cut -f 2 > /root/.bashrc2
+
+#Some more useful scripts and code
+wget http://www.trapkit.de/tools/checkrelro.sh -P /usr/share/arsenal/
+
+read -r -p "Want to use GEF? [y/N]" response4
+case "$response4" in
+    [yY][eE][sS]|[yY])
+
+	echo 'source /path/to/gef.py' >> ~/.gdbinit
+	esac
+
+
+read -r -p "Do you want to install IDA Freeware? [y/N]" response3
+case "$response3" in
+    [yY][eE][sS]|[yY])
+
+	echo "Installing IDA"
+
+	#Get IDA Freeware
+	wget https://out7.hex-rays.com/files/idafree70_linux.run -P /usr/share/arsenal/
+	sha1_sum=$(sha1sum /usr/share/arsenal/idafree70_linux.run | cut -d " " -f 1)
+	idafree70sha1=85984147fea9625fa149484eae2ef6d0c2739856
+
+	if [ "$sha1_sum" != "$idafree70sha1" ]
+
+	then
+        	echo "WARNING: Checksums don't match!\n"
+        	echo "What you downloaded:"
+        	echo $sha1_sum
+        	echo "What it should be:"
+        	echo $idafree70sha1"\n"
+        	read -r -p "Do you want to continue? [y/N]" response2
+        	case "$response2" in
+        	[yY][eE][sS]|[yY])
+        	echo "Downloading dependencies."
+        	;;
+    	*)
+        	echo "Exiting..."
+        	exit 1;
+        	;;
+	esac
+	fi
+
+
+
+
+	dpkg --add-architecture i386
+	apt-get update
+	apt-get install -y libpcre3:i386 libxdmcp6:i386 libc6:i386 libffi6:i386 libxcb1:i386 libgcc1:i386 libxau6:i386 zlib1g:i386 libx11-6:i386
+	echo "Installing IDA Freeware"
+	chmod +x /usr/share/arsenal/idafree70_linux.run && /usr/share/arsenal/idafree70_linux.run
+        ;;
+    *)
+	echo "Alright! You can install it later manually."
+	exit 1;
+        ;;
+esac
+
+
